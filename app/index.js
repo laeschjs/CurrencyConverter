e05febe74ef358b17e5ba66c2060e61d272035eb
Joshua Laesch <laeschjs@rose-hulman.edu>
2018-10-08 22:21:55 -0500

    Made a container component

--- app/index.js
+++ app/index.js
@@ -1,21 +1,32 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app from index :)!</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app from index :)!</Text>
-      </View>
-    );
-  }
-}
+EStyleSheet.build({
+  $primaryBlue: '#4f6d7a',
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => <Home />;
